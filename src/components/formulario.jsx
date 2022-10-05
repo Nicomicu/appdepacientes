ece1b2033cade0ded729355862304b041b05c105
nicomicu <nicomicu8@gmail.com>
2022-10-05 12:43:16 -0300

    nuevo componente

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react"
+import Error from "./Error"
 
 const Formulario = ({ paciente, setPacientes }) => {
   const [nombre, setNombre] = useState("")
@@ -52,11 +53,7 @@ const Formulario = ({ paciente, setPacientes }) => {
       <form
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
-        {error && (
-          <div className="bg-red-700 text-white text-center p-3 uppercase font-bold mb-5 rounded-md">
-            <p> Todos los campos son obligatorios</p>
-          </div>
-        )}
+        {error && <Error mensaje=" Todos los campos son obligatorios" />}
         <div className="mb-5">
           <label htmlFor="mascota" className="block font-black uppercase">
             Nombre Mascota:
