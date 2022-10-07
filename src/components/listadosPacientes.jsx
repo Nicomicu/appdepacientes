8715d6646f253b82aee48acc9019fc66575de035
nicomicu <nicomicu8@gmail.com>
2022-10-07 11:10:12 -0300

    Se mejoro ListadoPacientes

--- src/components/listadosPacientes.jsx
+++ src/components/listadosPacientes.jsx
@@ -3,17 +3,34 @@ import Paciente from "./Paciente"
 const ListadosPacientes = ({ paciente }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5  md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center"> Listado Pacientes </h2>
-      <p className="text-xl mt-5 mb-5 text-center">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold text-xl">
-          Pacientes y Citas
-        </span>
-      </p>
+      {paciente && paciente.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            {" "}
+            Listado Pacientes{" "}
+          </h2>
+          <p className="text-xl mt-5 mb-5 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold text-xl">
+              Pacientes y Citas
+            </span>
+          </p>
 
-      {paciente.map((paciente) => (
-        <Paciente key={paciente.id} paciente={paciente} />
-      ))}
+          {paciente.map((paciente) => (
+            <Paciente key={paciente.id} paciente={paciente} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-5 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold text-xl">
+              y apareceran en este lugar
+            </span>
+          </p>
+        </>
+      )}
     </div>
   )
 }
